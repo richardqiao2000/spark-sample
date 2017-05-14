4614e57b71e9aa608e117b5db7eb7fb5a898d992
richardqiao2000 <richardqiao2000@gmail.com>
2017-05-14 05:30:51 -0500

    init

---
+++ src/main/java/com/ibeifeng/sparkproject/dao/IAdClickTrendDAO.java
@@ -0,0 +1,17 @@
+package com.ibeifeng.sparkproject.dao;
+
+import java.util.List;
+
+import com.ibeifeng.sparkproject.domain.AdClickTrend;
+
+/**
+ * 广告点击趋势DAO接口
+ * 
+ * @author Administrator
+ *
+ */
+public interface IAdClickTrendDAO {
+
+  void updateBatch(List<AdClickTrend> adClickTrends);
+
+}
